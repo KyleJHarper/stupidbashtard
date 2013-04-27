1fb36df4629cc16c762ba8bacb7480077fda4dfb
administrator <administrator@ubuntu-12.04>
2013-04-26 23:42:00 -0500

    Created bin and lib directories to organize things better.

---
+++ bin/docker.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+
+# +---------------------------+
+# |  Pre-Requisites & Checks  |
+# +---------------------------+
+
+# Import required items
+use strict ;
+use warnings ;
+use Getopt::Std ;
+
+
+# +--------+
+# |  Main  |
+# +--------+
+print "hello\n" ;
+
+
+# +----------------+
+# |  Sub-Routines  |
+# +----------------+
