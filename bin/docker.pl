87f0517987aa2967e207c04790fe145653a3a759
administrator <administrator@ubuntu-12.04>
2013-04-27 00:14:46 -0500

    Started getting basics in place for docker.  Usage, printing, etc.  Still need to test the print_so function's respect for if QUIET logic.

--- bin/docker.pl
+++ bin/docker.pl
@@ -1,22 +1,41 @@
 #!/usr/bin/perl
 
 
-# +---------------------------+
-# |  Pre-Requisites & Checks  |
-# +---------------------------+
-
+# +------------------+
+# |  Pre-Requisites  |
+# +------------------+
 # Import required items
 use strict ;
 use warnings ;
 use Getopt::Std ;
+use Cwd 'abs_path' ;
+use File::Basename ;
 
+
+# +-----------------------+
+# |  Variables & Getopts  |
+# +-----------------------+
+my $LIB_DIR = dirname ( abs_path( __FILE__ ) ) . "/../lib" ;
+my $QUIET   = "" ;
 
 # +--------+
 # |  Main  |
 # +--------+
-print "hello\n" ;
+# -- Preflight Checks
+if ( ! -d $LIB_DIR ) { die "error:  Cannot find lib dir and no specific files listed.  Aborting." ; }
+
+# -- Read Ze Files
+print "\n" ;
 
 
 # +----------------+
 # |  Sub-Routines  |
 # +----------------+
+sub usage {
+  print
+}
+
+sub print_so {
+  # This exists to print things to stdout, but respects the -q flag
+  if ( $QUIET ) { return 0 ; }
+}
