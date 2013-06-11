ceb948b13eacc1574b25d4e7635b1a2be9123f52
administrator <administrator@ubuntu-12.04>
2013-06-10 22:08:09 -0500

    Added logic to save function.  Still need to debate on how much to save and where to save it.  Right now I have a name and tags... that's about it.

--- bin/bla.pl
+++ bin/bla.pl
@@ -1,17 +1,9 @@
 #!/usr/bin/perl
 
-if ( 1 ge 3 - 1 ) { print "hello\n" ; }
+my $fh;
 
-exit ;
+open($fh, '>>', "testfile.out");
 
-while (<>) {
-  chomp;
+print $fh "Some output" . " and more";
 
-  if ( /^[\s]*$/ ) { print "Skipping a line\n" ; next ; }
-  if ( /^[\s]*#@/ ) { print "This line has a tag\n" ; next ; }
-
-  print "Before: " . $_ . "\n" ;
-  s/[\s]*#(?!@).*// ;
-
-  print "After: " . $_ . "\n" ;
-}
+close($fh);
