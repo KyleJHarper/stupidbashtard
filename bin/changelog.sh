3411762d6b2b69f0d1bc37b7e19dff18f21e6332
kyle <kylejharper@gmail.com>
2013-07-15 17:56:47 -0500

    Updated the changelog script to be more properly aware of its own location.

--- bin/changelog.sh
+++ bin/changelog.sh
@@ -3,4 +3,4 @@
 # Copyright 2013 Kyle Harper
 # Licensed per the details in the LICENSE file in this package.
 
-git log --pretty=format:'%H%n  Author: %cn%n  Date: %ci%n  Comment: %s%n%n' --graph > ../CHANGELOG
+git log --pretty=format:'%H%n  Author: %cn%n  Date: %ci%n  Comment: %s%n%n' --graph > "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../CHANGELOG"
