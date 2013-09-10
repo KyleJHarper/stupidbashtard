c467d39ae660c63099601c9ff0e408e163004962
kyle <kylejharper@gmail.com>
2013-09-09 20:48:42 -0500

    Updated the namespace tester to test multiple namespaces at once.

--- tests/namespace.sh
+++ tests/namespace.sh
@@ -3,13 +3,10 @@
 # Copyright 2013 Kyle Harper
 # Licensed per the details in the LICENSE file in this package.
 
-# Run an entire namespace's worth of tests
-ns="${1}"
-if [ -z "${ns}" ] ; then echo "You have to specify a namespace to test." >&2 ; exit 1 ; fi
-if [ ! -d ${ns} ] ; then echo "Namespace specified doesn't exist."       >&2 ; exit 1 ; fi
-shift
-
 # Run each test.
-for file in ${ns}/* ; do
-  ./${file} ${1}
+for dir in $@ ; do
+  if [ ! -d "${dir}" ] ; then echo "Namespace specified doesn't exist."       >&2 ; exit 1 ; fi
+  for file in ${dir}/* ; do
+    ./${file} ${1}
+  done
 done
