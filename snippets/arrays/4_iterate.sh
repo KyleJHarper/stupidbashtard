b927e715c4e5f65c7f366efe1a7dcbb13d96f760
Kyle Harper <KyleJHarper@gmail.com>
2017-01-08 20:35:33 -0800

    More touch ups.

--- snippets/arrays/4_iterate.sh
+++ snippets/arrays/4_iterate.sh
@@ -20,6 +20,7 @@ for item in "${ary[*]}" ; do   # ==> for item in 'item 1 item 2'
   echo "${item}"
 done
 
+
 # Reverse an array
 printf '\n%s\n' '[Reverse an array and store it]'
 ary_rev=()
@@ -27,4 +28,21 @@ for item in "${ary[@]}" ; do
   ary_rev=("${item}" "${ary_rev[@]}")
 done
 echo "${ary_rev[@]}"   # ==> echo 'item 2' 'item 1'
+
+
+# Work on array in reverse without storing it like above.
+printf '\n%s\n' '[Operate on array in reverse without storing it]'
+ary_with_holes=('item 1' 'item 2' 'item 3' 'item 4' 'item 5')
+unset ary_with_holes[2]
+unset ary_with_holes[4]
+highest_index=0
+for index in "${!ary_with_holes[@]}" ; do
+  [ ${index} -gt ${highest_index} ] && highest_index=${index}
+done
+for (( i=${highest_index}; i>=0; i-- )) ; do
+  if [ "${ary_with_holes[$i]+i am real}" == 'i am real' ] ; then
+    echo "I went in reverse... ${ary_with_holes[$i]}"
+    # Do whatever logic you want here...
+  fi
+done
 
